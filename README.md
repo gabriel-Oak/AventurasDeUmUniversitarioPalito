598a2dbeb7a4a2b0c608c758b270032af0973a58
oTrolado <gabriel.costa41@fatec.sp.gov.br>
2017-05-19 18:10:08 -0300

    Primeiras cenas

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # Aventuras De Um Universitario Palito
 Parabens! Você chegou a Fatec, mas esse é só o inicio
 de sua aventura!!
-
-Ps ainda vou upar uma pasta com as imagens que devem ser colocadas na pasta "assets/img/"
--- assets/jS/main.js
+++ assets/jS/main.js
@@ -1,1 +1,54 @@
 alert("Bem Vindo! Você agora engressou na fatec!");
+var cenaAtual = 0;
+
+function trocaImg(){
+	document.getElementById("cena").src = "assets/img/cena" + cenaAtual + ".jpeg";
+}
+
+function escolhe(){
+	cenaAtual = prompt("Digite o numero da ceda");
+	trocaImg();
+}
+
+function mudaCena (escolha){
+	switch (cenaAtual){
+		case 0:
+			cenaAtual = 1;
+  			trocaImg();
+  			alert("Você chegou ao portão de entrada depois de pegar 3 ônibus lotados");
+  			break;
+  		case 1:
+  			if (escolha == 1){
+  				cenaAtual = 2;
+  				alert("Entrando...");
+  				trocaImg();
+  			}
+  			else{
+  				cenaAtual = 3;
+  				alert("Cheiro de... COMIDA!");
+  				trocaImg();
+  			}
+  			break;
+  		case 2:
+  			if (escolha == 1){
+  				alert("Boa noite para você também!");
+  			}
+  			else {
+  				alert("Mawn!!");
+  			}
+  			break;
+  		case 3:
+  			if (escolha == 1){
+  				cenaAtual = 4;
+  				alert("Isso é uma coxinha ou pastel? O diretor de arte é horrível");
+  				trocaImg();
+  			}
+  			else {
+  				cenaAtual = 5;
+  				alert("Conversando... e conversando mais...");
+  				trocaImg();
+  			}
+  			break;
+	}
+}
+
