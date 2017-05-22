8ee2d32336a60c3371fe968568b0866b8d09b69a
oTrolado <gabriel.costa41@fatec.sp.gov.br>
2017-05-22 14:29:50 -0300

    Melhoriais de layout

--- assets/jS/main.js
+++ assets/jS/main.js
@@ -1,9 +1,30 @@
-var mensagem = "<h2>Bem Vindo! Você agora engressou na fatec!</h2>";
+var mensagem = "Bem Vindo! Você agora engressou na fatec!";
 var cenaAtual = 0;
-
+ 
 function trocaImg(){
+  var node = document.getElementById("descricao");
+  var x = 0;
 	document.getElementById("cena").src = "assets/img/cena" + cenaAtual + ".jpeg";
-  document.getElementById("textoCena").innerHTML = mensagem;
+  //document.getElementById("textoCena").innerHTML = mensagem;
+  //innerHTML é o metodo mais facíl porem não o oficial.
+  while (node.firstChild)
+    node.removeChild(node.firstChild);
+
+  node.appendChild(document.createTextNode(mensagem));
+}
+
+function botao(id, texto, reseta){
+  var node = document.getElementById(id);
+  while (node.firstChild)
+    node.removeChild(node.firstChild);
+  node.appendChild(document.createTextNode(texto));
+
+  if (reseta == true){
+    node.className = "undec";
+  } else {
+    node.className = "decisao";
+  }
+
 }
 
 function escolhe(){
@@ -15,71 +36,91 @@ function mudaCena (escolha){
 	switch (cenaAtual){
 		case 0:
 			  cenaAtual = 1;
-        mensagem = "<h2>Você chegou ao portão de entrada depois de pegar 3 ônibus lotados</h2>";
+        mensagem = "Você chegou ao portão de entrada depois de pegar 3 ônibus lotados";
   			trocaImg();
+        botao("escolha1", "Entrar");
+        botao("escolha2", "Cantina!");
   			break;
   		case 1:
   			if (escolha == 1){
   				cenaAtual = 2;
-  				mensagem = "<h2>Entrando...</h2>";
+  				mensagem = "Entrando...";
   				trocaImg();
+          botao("escolha1", "Boa Noite!");
+          botao("escolha2", "Gatinho, gatinho");
   			}
   			else{
   				cenaAtual = 3;
-  				mensagem = "<h2>Cheiro de... COMIDA!</h2>";
+  				mensagem = "Cheiro de... COMIDA!";
   				trocaImg();
+          botao("escolha1", "Comer");
+          botao("escolha2", "Conversar");
   			}
   			break;
   		case 2:
   			if (escolha == 1){
   				alert("Boa noite para você também!");
-          mensagem = "<h2>Chegamos no laboratório</h2";
+          mensagem = "Chegamos no laboratório";
           trocaImg();
+          //botoes
   			}
   			else {
   			  alert("Mawn!!");
-          mensagem = "<h2>Chegamos no laboratório</h2";
+          mensagem = "Chegamos no laboratório";
           trocaImg();
+          //botoes
   			}
   			break;
   		case 3:
   			if (escolha == 1){
   				cenaAtual = 4;
-  				mensagem = "<h2>Isso é uma coxinha ou pastel? O diretor de arte é horrível</h2>";
+  				mensagem = "Isso é uma coxinha ou pastel? O diretor de arte é horrível";
   				trocaImg();
+          botao("escolha1", "Avançar!");
+          botao("escolha2", "", true);
   			}
   			else {
   				cenaAtual = 5;
-  				mensagem = "<h2>Conversando... e conversando mais...</h2>";
+  				mensagem = "Conversando... e conversando mais";
   				trocaImg();
+          botao("escolha1", "Avançar");
+          botao("escolha2", "", true);
   			}
   			break;
       case 4:
         if (escolha){
           cenaAtual = 6;
-          mensagem = "<h2>Você ficou pobre e agora vai ter de vender miçanga!!</h2>";
+          mensagem = "Você ficou pobre e agora vai ter de vender miçanga!!";
           trocaImg();
+          botao("escolha1", "Entrar -->");
+          botao("escolha2", "", true);
         }
         break;
       case 5:
         if (escolha){
           cenaAtual = 7;
-          mensagem = "<h2>Você acaba de desenvolver um câncer</h2>";
+          mensagem = "Você acaba de desenvolver um câncer";
           trocaImg();
+          botao("escolha1", "Entrar -->");
+          botao("escolha2", "", true);
         }
         break;
       case 6:
         if (escolha){
           cenaAtual = 2;
-          mensagem = "<h2>Entrando...</h2>";
+          mensagem = "Entrando...";
           trocaImg();
+          botao("escolha1", "Boa Noite!");
+          botao("escolha2", "Gatinho, gatinho");
         }
         break;
       case 7:
         if (escolha){
           cenaAtual = 2;
-          mensagem = "<h2>Entrando...</h2>";
+          mensagem = "Entrando...";
           trocaImg();
+          botao("escolha1", "Boa Noite!");
+          botao("escolha2", "Gatinho, gatinho");
         }
         break;
 
